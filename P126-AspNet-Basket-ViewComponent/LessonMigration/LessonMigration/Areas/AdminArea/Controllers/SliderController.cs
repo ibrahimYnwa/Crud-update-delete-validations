4f263004cb7480cae1924980627adecdf0348173
ibrahim <ibrahimea@code.edu.az>
2022-03-29 00:01:06 +0400

    added

---
+++ P126-AspNet-Basket-ViewComponent/LessonMigration/LessonMigration/Areas/AdminArea/Controllers/SliderController.cs
@@ -0,0 +1,27 @@
+﻿using LessonMigration.Data;
+using LessonMigration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMigration.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+      
+    public async Task <IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.AsNoTracking().ToListAsync();
+            return View(sliders);
+        }
+    }
+}
